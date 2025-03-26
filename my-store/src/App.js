39117da13b2b0b4cc414a331446e75dde784a1ca
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-03-26 15:34:54 +0100

    Imported all the pages into the App.js file

--- my-store/src/App.js
+++ my-store/src/App.js
@@ -1,10 +1,28 @@
+import { Route, Routes } from "react-router-dom";
+
+
+
+import Home from "./pages/Home";
+import Products from './pages/Products';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Cart from './pages/Cart';
+import LoginSignup from './pages/LoginSignup'
 
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome Papa Dimmy</h1>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="products" element={<Products />} />
+        <Route path="about-us" element={<About />} />
+        <Route path="contact-us" element={<Contact />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="login-signup" element={<LoginSignup />} />
+      </Routes>
+
+    </>
   );
 }
 
