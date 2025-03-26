bcad5994f08abb074404aa7defaa6e623d762665
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-03-26 15:04:37 +0100

    created all_products, new_collections and best_sellers files in the assets/images folder

---
+++ my-store/src/assets/images/new_collections.js
@@ -0,0 +1,70 @@
+
+
+let new_collections = [
+    {
+        id: 2,
+        name: 'ALISISTER Men 3D Hoodie',
+        image: '',
+        old_price: 120.50,
+        new_price: 90.00,
+        category: 'hoodie',
+    },
+    {
+        id: 61,
+        name: 'Men Mesh Baseball Cap',
+        image: '',
+        old_price: 50.00,
+        new_price: 40.00,
+        category: 'headwear',
+    },
+    {
+        id: 44,
+        name: 'Men Green Striped Short Sleeve',
+        image: '',
+        old_price: 70.00,
+        new_price: 55.00,
+        category: 't-shirt',
+    },
+    {
+        id: 45,
+        name: 'Faith Christian Shirt',
+        image: '',
+        old_price: 60.00,
+        new_price: 55.00,
+        category: 't-shirt',
+    },
+    {
+        id: 27,
+        name: 'TOLOER Pullover Hoodie',
+        image: '',
+        old_price: 100.00,
+        new_price: 80.00,
+        category: 'hoodie',
+    },
+    {
+        id: 77,
+        name: 'Fashion Colorblock Lace-up Sneakers',
+        image: '',
+        old_price: 80.00,
+        new_price: 65.00,
+        category: 'footwear',
+    },
+    {
+        id: 93,
+        name: 'Moccasin Glitter Male Footwear',
+        image: '',
+        old_price: 70.00,
+        new_price: 65.00,
+        category: 'footwear',
+    },
+    {
+        id: 32,
+        name: 'Men Solid Round Neck Tee',
+        image: '',
+        old_price: 60.00,
+        new_price: 55.00,
+        category: 't-shirt',
+    },
+]
+
+export default new_collections;
