36361dde0c3736f78ac1c4177a98127dd95449da
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-03-27 10:12:44 +0100

    Created the NavLinks in the NavBar and imported the NavBar in the Home file

--- my-store/src/components/NavBar.js
+++ my-store/src/components/NavBar.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 function NavBar() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <nav>
+        <ul>
+            <NavLink>Home</NavLink>
+            <NavLink>Products</NavLink>
+            <NavLink>About</NavLink>
+            <NavLink>Contact</NavLink>
+            <NavLink>Cart</NavLink>
+            <NavLink>Login</NavLink>
+        </ul>
+      </nav>
+    </>
   )
 }
 
