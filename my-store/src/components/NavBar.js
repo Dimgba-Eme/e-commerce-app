9624eef1db72a2827872d2183c99793bc71aa820
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-03-27 15:54:01 +0100

    Positioned and styled the mobile view of the header and its contents

--- my-store/src/components/NavBar.js
+++ my-store/src/components/NavBar.js
@@ -1,23 +1,27 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import close_btn from '../assets/icons/close_icon.png'
 
 
-function NavBar() {
-  return (
-    <>
 
-      <nav>
-        <ul>
-            <NavLink>Home</NavLink>
-            <NavLink>Products</NavLink>
-            <NavLink>About</NavLink>
-            <NavLink>Contact</NavLink>
-            <NavLink>Cart</NavLink>
-            <NavLink>Login</NavLink>
-        </ul>
-      </nav>
-    </>
-  )
+function NavBar(props) {
+    const { closeBtn } = props;
+    return (
+        <>
+
+            <nav className='nav-container isOpen' onClick={() => closeBtn()}>
+                <img src={close_btn} alt='' onClick={() => closeBtn()} className='close-btn' />
+                <ul className='nav-inner'>
+                    <NavLink className='nav-link'>Home</NavLink>
+                    <NavLink className='nav-link'>Products</NavLink>
+                    <NavLink className='nav-link'>About</NavLink>
+                    <NavLink className='nav-link'>Contact</NavLink>
+                    {/* <NavLink className='nav-link'>Cart</NavLink> */}
+                    <NavLink className='nav-link'>Login</NavLink>
+                </ul>
+            </nav>
+        </>
+    )
 }
 
 export default NavBar
