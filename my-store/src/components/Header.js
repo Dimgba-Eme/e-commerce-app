a827873edf669753d7d8436b045fab465507308c
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-03-28 20:19:44 +0100

    Moved the cart icon and menu icon to the NavBar component

--- my-store/src/components/Header.js
+++ my-store/src/components/Header.js
@@ -1,35 +1,20 @@
 import './header.scss'
-import React, { useState } from 'react'
 import Logo from '../assets/icons/logo.png'
-import cart_icon from '../assets/icons/cart_icon.png'
-import menu_btn from '../assets/icons/hamburger_menu.svg'
 import NavBar from '../components/NavBar'
 
 
 
 function Header() {
-    const [isOpen, setIsOpen] = useState(false)
-
-    const closeBtn = () => {
-        return (
-            setIsOpen(!isOpen)
-        )
-    }
     return (
         <>
 
             <header className='header-wrapper'>
-                <img src={menu_btn} alt='hamburger menu' className='menu-btn' onClick={() => setIsOpen(!isOpen)} />
                 <div className='header-container'>
                     <img src={Logo} alt='Logo' className='logo' />
                     <h4>Dimmy's Store</h4>
                 </div>
-                {isOpen && (
-                    <NavBar closeBtn={closeBtn} />
-                )}
-
-
-                <img src={cart_icon} alt='cart icon' className='cart-icon' />
+        
+                    <NavBar />
             </header>
 
         </>
--- my-store/src/components/NavBar.js
+++ my-store/src/components/NavBar.js
@@ -1,25 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
+import './navBar.scss'
+import cart_icon from '../assets/icons/cart_icon.png'
+import menu_btn from '../assets/icons/hamburger_menu.svg'
 import close_btn from '../assets/icons/close_icon.png'
 
 
 
-function NavBar(props) {
-    const { closeBtn } = props;
+function NavBar() {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <>
+            <img src={menu_btn} alt='hamburger menu' className='menu-btn' onClick={() => {setIsOpen(!isOpen) }} />
 
-            <nav className='nav-container isOpen' onClick={() => closeBtn()}>
-                <img src={close_btn} alt='' onClick={() => closeBtn()} className='close-btn' />
+
+            <nav className={isOpen ? 'isOpen' : ''} onClick={() => {setIsOpen(!isOpen) }}>
                 <ul className='nav-inner'>
+                <img src={close_btn} alt='' onClick={() => { setIsOpen(!isOpen)}} className='close-btn' />
                     <NavLink className='nav-link'>Home</NavLink>
                     <NavLink className='nav-link'>Products</NavLink>
                     <NavLink className='nav-link'>About</NavLink>
                     <NavLink className='nav-link'>Contact</NavLink>
-                    {/* <NavLink className='nav-link'>Cart</NavLink> */}
                     <NavLink className='nav-link'>Login</NavLink>
+
                 </ul>
             </nav>
+            <img src={cart_icon} alt='cart icon' className='cart-icon' />
         </>
     )
 }
