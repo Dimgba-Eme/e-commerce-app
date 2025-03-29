95420c40df90fa60b056117bb89524e22a56f1a0
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-03-29 10:20:31 +0100

    Imported the hero image into the Hero component

--- my-store/src/components/Hero.js
+++ my-store/src/components/Hero.js
@@ -1,11 +1,26 @@
 import React from 'react'
+import HeroImage from '../assets/images/hero_image.jpeg'
 
 function Hero() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <>
+            <section className='hero-container'>
+                <div className='hero-inner'>
+
+                    <div className='hero-offer'>
+                        <h1>SPECIAL OFFER!!!</h1>
+                        <h1>50% OFF ON ALL SALES</h1>
+                        <p>Take advantage of this amazing offer!</p>
+                    </div>
+
+                    <div className='hero-image-container'>
+                        <img src={HeroImage} className='hero-image' />
+                    </div>
+                </div>
+                <button>SHOP NOW</button>
+            </section>
+        </>
+    )
 }
 
 export default Hero
