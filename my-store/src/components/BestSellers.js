c7f69b9181b7e76c11bd639325340b6f39628999
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-03-29 16:06:51 +0100

    Properly displayed and aligned the best_sellers data from the server

--- my-store/src/components/BestSellers.js
+++ my-store/src/components/BestSellers.js
@@ -4,27 +4,31 @@ import './bestSellers.scss'
 import best_sellers from '../assets/images/best_sellers'
 
 function BestSellers() {
-  return (
-    <>
-      <section className='best-sellers-container'>
-        <div className='best-sellers-title'>
-            <h1>Best Sellers</h1>
-            <hr />
-        </div>
-        <div className='best-sellers-container'>
-           {best_sellers.map((items) => {
-            return (
-                <div key={items.id}>
-                    <img src={items.image} alt='Best sellers products' />
-                    <h4>{items.name}</h4>
-                    <p>${items.new_price}</p>
+    return (
+        <>
+            <section className='best-sellers-container'>
+                <div className='best-sellers-title'>
+                    <h1>Best Sellers</h1>
+                    <hr />
                 </div>
-            )
-           })}
-        </div>
-      </section>
-    </>
-  )
+                <div className='best-sellers-data-wrapper'>
+                    {best_sellers.map((items) => {
+                        return (
+                            <>
+
+                                <div key={items.id} className='best-sellers-data'>
+                                    <img src={items.image} alt='Best sellers products' />
+                                    <h4>{items.name}</h4>
+                                    <p>${items.new_price}</p>
+                                </div>
+                            </>
+
+                        )
+                    })}
+                </div>
+            </section>
+        </>
+    )
 }
 
 export default BestSellers
