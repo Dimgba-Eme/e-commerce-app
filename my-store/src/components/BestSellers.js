f33d78d8fdbc271a098962e62c5a45b84a47c769
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-03-29 14:36:39 +0100

    Fetched the best_sellers data into the BestSeller section with the map function

--- my-store/src/components/BestSellers.js
+++ my-store/src/components/BestSellers.js
@@ -11,8 +11,16 @@ function BestSellers() {
             <h1>Best Sellers</h1>
             <hr />
         </div>
-        <div>
-           {best_sellers}
+        <div className='best-sellers-container'>
+           {best_sellers.map((items) => {
+            return (
+                <div key={items.id}>
+                    <img src={items.image} alt='Best sellers products' />
+                    <h4>{items.name}</h4>
+                    <p>${items.new_price}</p>
+                </div>
+            )
+           })}
         </div>
       </section>
     </>
