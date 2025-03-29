d13a6f0aa35124128202f03bdb59be1f3a519ceb
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-03-29 13:23:40 +0100

    Created the Items component which will be used to display data from the server

--- my-store/src/components/BestSellers.js
+++ my-store/src/components/BestSellers.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import './bestSellers.scss'
 
+import best_sellers from '../assets/images/best_sellers'
+
 function BestSellers() {
   return (
     <>
       <section className='best-sellers-container'>
         <div className='best-sellers-title'>
             <h1>Best Sellers</h1>
+            <hr />
         </div>
         <div>
 
