79ac2b3cbcb09d9398e62e242a6131d74d9d0e28
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-03-30 20:22:11 +0100

    Created and imported the Newsletter component into the Home page

---
+++ my-store/src/components/Newsletter.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Newsletter = () => {
+    return (
+        <>
+            <section>
+                <div>
+                    <h1>Subscribe To Our Newsletter</h1>
+                    <p></p>
+                </div>
+
+            </section>
+        </>
+    )
+}
+
+export default Newsletter
--- my-store/src/pages/Home.js
+++ my-store/src/pages/Home.js
@@ -4,6 +4,7 @@ import Hero from '../components/Hero'
 import NewCollections from '../components/NewCollections'
 import BestSellers from '../components/BestSellers'
 import Categories from '../components/Categories'
+import Newsletter from '../components/Newsletter'
 const Home = () => {
     return (
         <>
@@ -12,6 +13,7 @@ const Home = () => {
                 <NewCollections />
                 <BestSellers />
                 <Categories />
+                <Newsletter />
             </main>
         </>
     )
