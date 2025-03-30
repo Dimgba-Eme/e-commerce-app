73478727d4b41d734123ed063968ecf9cce9eba7
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-03-30 21:49:14 +0100

    Created and imported newsletter.scss file into the Newsletter component for styling

--- my-store/src/components/Newsletter.js
+++ my-store/src/components/Newsletter.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import './newsletter.scss'
 
 const Newsletter = () => {
     return (
         <>
-            <section>
-                <div>
+            <section className='newsletter-container'>
+                <div className='title-container'>
                     <h1>Subscribe To Our Newsletter</h1>
-                    <p></p>
+                    <p>Let us inform and update you on all important and latest information. </p>
+                </div>
+
+                <div className='input-container'>
+                    <input type='email' value='' onChange='' placeholder='Email' id='email' />
+                    <button>Subscribe</button>
                 </div>
 
             </section>
