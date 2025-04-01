3f7a2eb15e94e9bd040441ea3ecd82ebe23bdc30
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-01 12:47:23 +0100

    Styled and aligned the link tabs in the Products page

--- my-store/src/components/ProductsTabs.js
+++ my-store/src/components/ProductsTabs.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './productsTabs.scss'
 
 const ProductsTabs = () => {
   return (
     <>
       <nav className='products-tab'>
         <ul className='products-tab-inner'>
-          <Link to='hoodies' className='products-tab'>Hoodies</Link>
-          <Link to='t-shirt' className='products-tab'>T-shirt</Link>
-          <Link to='headwear' className='products-tab'>Headwear</Link>
-          <Link to='footwear' className='products-tab'>Footwear</Link>
-          <Link to='accessories' className='products-tab'>Accessories</Link>
+          <Link to='hoodies' className='tab'>Hoodies</Link>
+          <Link to='t-shirt' className='tab'>T-shirt</Link>
+          <Link to='headwear' className='tab'>Headwear</Link>
+          <Link to='footwear' className='tab'>Footwear</Link>
+          <Link to='accessories' className='tab'>Accessories</Link>
         </ul>
       </nav>
     </>
