4f8d9c0057b4e19cf07479246f19f36318c091d6
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-02 11:26:11 +0100

    Imported ProductsDisplay component into SingleItem component for grabbing items to display

--- my-store/src/components/SingleItem.js
+++ my-store/src/components/SingleItem.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import all_products from '../assets/images/all_products'
+import { useParams } from 'react-router-dom'
+import ProductsDisplay from './ProductsDisplay'
+import { useProducts } from '../contexts/ContextProvider'
 
 const SingleItem = () => {
-  return (
-    <div>
-      <h1>Single Item</h1>
-    </div>
-  )
+    const { all_products } = useProducts();
+    const { productId } = useParams();
+    const product = all_products.find((item) => item.id === Number(productId))
+    return (
+        <main>
+            <h1>Single Item</h1>
+            <ProductsDisplay product={product} />
+        </main>
+    )
 }
 
 export default SingleItem
