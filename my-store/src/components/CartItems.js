5d1a61667e835ba3ac85e21b6d678b27ab0cb3dd
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-03 17:57:52 +0100

    Fetched all the necessary data from ContextProvider component to CartItems file

--- my-store/src/components/CartItems.js
+++ my-store/src/components/CartItems.js
@@ -3,7 +3,7 @@ import './cartItems.scss'
 import { useProducts } from '../contexts/ContextProvider'
 
 const CartItems = () => {
-    const {all_products, cartItems, addToCart, removeFromCart, deleteFromCart } = useProducts();
+    const { all_products, cartItems, addToCart, removeFromCart, deleteFromCart, getTotalCartAmount } = useProducts();
     return (
         <section className='cart-items-container'>
             <h1>Shopping Cart</h1>
@@ -18,35 +18,43 @@ const CartItems = () => {
                 </div>
                 <hr />
 
-                <div className='cart-items-wrapper'>
-                    <div>
-                        <img />
-                    </div>
+                {all_products.map((item) => {
+                    if (cartItems[item.id] > 0) {
+                        return (
+                            <div className='cart-items-wrapper'>
+                                <div>
+                                    <img src={item.image} />
+                                </div>
 
-                    <div>
-                        <h4>title</h4>
-                    </div>
+                                <div>
+                                    <h4>{item.name}</h4>
+                                </div>
 
-                    <div>
-                        <p>price</p>
-                    </div>
+                                <div>
+                                    <p>${item.new_price}</p>
+                                </div>
 
-                    <div>
-                        <button>-</button>
-                        <button>0</button>
-                        <button>+</button>
-                    </div>
+                                <div>
+                                    <button onClick={removeFromCart(item.id)}>-</button>
+                                    <button>{cartItems[item.id]}</button>
+                                    <button onClick={addToCart(item.id)}>+</button>
+                                </div>
 
-                    <div>
-                        <h4>total</h4>
-                    </div>
+                                <div>
+                                    <h4>total</h4>
+                                </div>
 
-                    <div>
-                        <img />
-                    </div>
+                                <div>
+                                    <img />
+                                </div>
 
-                    <hr />
-                </div>
+                                <hr />
+                            </div>
+                        )
+                    }
+                })}
+
+
             </div>
 
 
