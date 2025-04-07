2efcfb9d64f5d191e2b5ff7cf5642e1a796b7e1f
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-07 10:36:14 +0100

    Added and Imported an image for the about page

--- my-store/src/pages/About.js
+++ my-store/src/pages/About.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import './about.scss'
-
+import about_hero from '../assets/images/about_hero.png'
 const About = () => {
   return (
     <main className='about-container'>
       <h1>About Us</h1>
       <div className='about-inner'>
-         <img />
+        <img src={about_hero} alt='' />
+        <div className='about-description'>
+          <p>Dimmy's Store was born out of a passion for innovation and a desire to revolutionize
+            the way people shop online. Our journey began with a simple idea: to provide
+            a platform where customers can easily discover, explore, and purchase a wide
+            range of products from the comfort of their homes.
+            Since our inception, we've worked tirelessly to curate a diverse selection
+            of high-quality products that cater to every taste and preference.</p>
+          <h4>Our Mission</h4>
+          <p>Our mission at Dimmy's Store is to empower customers with choice, convenience,
+            and confidence. We're dedicated to providing a seamless shopping experience
+            that exceeds expectations, from browsing and ordering to delivery and beyond.</p>
+        </div>
       </div>
     </main>
   )
