549d93dcad2203f17f076ecec343e37f03d65e15
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-07 21:23:07 +0100

    Built an error function for the password field when an error occurs

--- my-store/src/components/Login.js
+++ my-store/src/components/Login.js
@@ -1,23 +1,57 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './login.scss'
 import { validateEmail } from './utils';
 
+const PasswordErrorMessage = () => {
+    return (
+        <p style={{ color: 'red' }}>Password must not be less eight characters</p>
+    )
+}
+
 const Login = () => {
-    const [email, setEmail] = useState();
+    const [email, setEmail] = useState("");
     const [password, setPassword] = useState({
         value: '',
         isFocused: false,
     });
 
+    const isFormValid = () => {
+        return (
+            email && validateEmail(email) && password.value.length >= 8
+        )
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        clearForm();
+        alert("Logged In")
+    }
+
+    const clearForm = () => {
+        setEmail("");
+        setPassword({
+            value: '',
+            isFocused: false,
+        })
+    }
+    
+    const inputRef = useRef();
+
+    useEffect(() => {
+        document.title = "Login | Dimmy's Store"
+        inputRef.current.focus();
+    })
+
     return (
         <section className='login-container'>
             <div className='login-inner'>
-                <form>
-                    <input type='email' id='email' placeholder='Enter email' value={email} onChange={(e) => setEmail(e.target.value)} />
+                <form onSubmit={handleSubmit}>
+                    <input type='email' ref={inputRef} id='email' placeholder='Enter email' value={email} onChange={(e) => setEmail(e.target.value)} />
                     <br />
                     <input type='password' id='pass' placeholder='Enter password' value={password.value} onChange={(e) => setPassword({ ...password, value: e.target.value })} onBlur={(e) => setPassword({ ...password, isFocused: true })} />
+                    {password.isFocused && password.value.length < 8 ? (<PasswordErrorMessage />) : null}
                     <br />
-                    <button type='submit'>Sign In</button>
+                    <button type='submit' disabled={!isFormValid()}>Sign In</button>
                     <div>
                         <p>Forgot password?</p>
                         <p>Don't have and account? Sign up here</p>
