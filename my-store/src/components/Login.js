f26badb53738c5734435b4ba1fcdd00466bb6a65
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-07 18:22:20 +0100

    Setting up the login form with important input types

--- my-store/src/components/Login.js
+++ my-store/src/components/Login.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './login.scss'
 
 const Login = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState({
+        value: '',
+        isFocused: false,
+    });
+
+    return (
+        <section className='login-container'>
+            <div className='login-inner'>
+                <form>
+                    <input type='email' id='email' placeholder='Enter email' value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <br />
+                    <input type='password' id='pass' placeholder='Enter password' value={password.value} onChange={(e) => setPassword({ ...password, value: e.target.value })} onBlur={(e) => setPassword({ ...password, isFocused: true })} />
+                    <br />
+                    <button type='submit'>Sign In</button>
+                    <div>
+                        <p>Forgot password?</p>
+                        <p>Don't have and account? Sign up here</p>
+                    </div>
+                </form>
+            </div>
+        </section>
+    )
 }
 
 export default Login
+
