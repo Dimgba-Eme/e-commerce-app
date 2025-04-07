5c36f9b2f6a4d67414183bdfcedf736d322ca46b
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-07 14:21:25 +0100

    Imported the contact_us image into the Contact Page

--- my-store/src/pages/About.js
+++ my-store/src/pages/About.js
@@ -9,13 +9,13 @@ const About = () => {
   useEffect(() => {
     document.title = "About | Dimmy's Store"
   })
-  
+
   return (
     <main className='about-wrapper'>
 
       <div className='about-container'>
         <h1>About Us</h1>
-        <div className='about-inner'>
+        <section className='about-inner'>
           <img src={about_hero} alt='' />
           <div className='about-description'>
             <p>Dimmy's Store was born out of a passion for innovation and a desire to revolutionize
@@ -35,22 +35,23 @@ const About = () => {
               that exceeds expectations, from browsing and ordering to delivery and beyond.
             </p>
           </div>
-        </div>
+        </section>
+
         <h1>Why Choose Us</h1>
-        <div className='why-choose-container'>
-          <div className='why-us'>
+        <section className='why-choose-container'>
+          <article className='why-us'>
             <h4>Quality Assurance:</h4>
             <p>We meticulously select and vet each product to ensure it meets our stringent quality standards.</p>
-          </div>
-          <div className='why-us'>
+          </article>
+          <article className='why-us'>
             <h4>Convenience:</h4>
             <p>With our user-friendly interface and hassle-free ordering process, shopping has never been easier.</p>
-          </div>
-          <div className='why-us'>
+          </article>
+          <article className='why-us'>
             <h4>Exceptional Customer Service:</h4>
             <p>Our team of dedicated professionals is here to assist you all the way, ensuring your satisfaction is our top priority.</p>
-          </div>
-        </div>
+          </article>
+        </section>
       </div>
       <Newsletter />
     </main>
--- my-store/src/pages/Contact.js
+++ my-store/src/pages/Contact.js
@@ -1,11 +1,34 @@
 import React from 'react'
 import './contact.scss'
+import contact_us from '../assets/images/contact_us.png'
+import Newsletter from '../components/Newsletter'
 
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact</h1>
-    </div>
+    <main className='contact-wrapper'>
+      <h1>Contact Us</h1>
+      <section className='contact-inner'>
+        <img src={contact_us} alt='' />
+
+        <div className='contact-info'>
+          <h3>Our Store</h3>
+          <br />
+          <br />
+          <p>54709 Willms Station
+            Suite 350, Washington, USA
+          </p>
+          <br />
+          <br />
+          <p>Tel: +234 708 549 5430</p>
+          <p>Email: hello@dimgbaeme.com.ng</p>
+        </div>
+      </section>
+      <br />
+      <br />
+      <br />
+
+      <Newsletter />
+    </main>
   )
 }
 
